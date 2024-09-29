cd688b87e457bc1a387ed4eccb9c84f3fd72ab58
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-29 15:11:47 +0300

    ✏️ Models

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('sequelize-training', 'root', {
+const sequelize = new Sequelize('sequelize-training', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
 });
 
 
-sequelize.authenticate()
+// sequelize.authenticate()
+//     .then(() => {
+//         console.log('Sequelize authenticated successfully.');
+//     })
+//     .catch((err) => {
+//         console.error('Unable to connect to the database:', err);
+//     });
+
+const User = sequelize.define('User', {
+    username: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    age: {
+        type: Sequelize.DataTypes.INTEGER,
+        defaultValue: 21,
+    }
+});
+
+User.sync()
     .then(() => {
-        console.log('Sequelize authenticated successfully.');
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+        console.log('User successfully created!');
+    }).catch(err => {
+    console.log('User failed with error: ' + err)
+});
+
